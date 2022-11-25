5bde0af1b698646317905062121d0915628f8d07
Roman Hronza <roman.hronza@gmail.com>
2022-11-25 18:21:15 +0100

    add log message

--- src/main/java/cz/hronza/rhrpoceasybe/api/RhrpocEasyBeRestApi.java
+++ src/main/java/cz/hronza/rhrpoceasybe/api/RhrpocEasyBeRestApi.java
@@ -11,14 +11,23 @@ import javax.validation.Valid;
 
 @RestController
 public class RhrpocEasyBeRestApi {
+
+    private static final System.Logger log = System.getLogger(RhrpocEasyBeRestApi.class.getSimpleName());
+
     @GetMapping(path = { "/reverse-endpoint"})
     public ResponseEntity<OutputDto> makeReverze(
             @Valid @RequestParam (value = "id") String id,
             @Valid @RequestParam (value = "name") String name
             ) {
+        log.log(System.Logger.Level.INFO, "START");
+        log.log(System.Logger.Level.INFO, String.format("  ID=%s", id));
+        log.log(System.Logger.Level.INFO, String.format("  NAME=%s", name));
         String idReverse = new StringBuilder(id).reverse().toString();
         String nameReverse = new StringBuilder(name).reverse().toString();
         OutputDto outputDto = new OutputDto(idReverse, nameReverse);
+        log.log(System.Logger.Level.INFO, String.format("  outputDto=%s", outputDto));
+        log.log(System.Logger.Level.INFO, "END");
+
         return ResponseEntity.ok(outputDto);
     }
 
